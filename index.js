7d4763e7eb69b409506e3824a5c7cafa2f566e04
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-06 18:34:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ function findMatching(list, name) {
 }
 
 function fuzzyMatch(list, partialName) {
-  
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
